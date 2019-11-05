a1bdf61452ddfdb1d81ad9f7ef4e271293f5350d
1909654 <ngtl-wm17@student.tarc.edu.my>
2019-11-05 22:03:34 +0800

    Done

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +21,11 @@ class MainActivity : AppCompatActivity() {
     private fun makeColored(view: View) {
         when (view.id) {
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundResource(R.drawable.word)
-            R.id.box_two_text -> view.setBackgroundResource(R.drawable.robin)
-            R.id.box_three_text -> view.setBackgroundResource(R.drawable.katana)
-            R.id.box_four_text -> view.setBackgroundResource(R.drawable.shortgun)
-            R.id.box_five_text -> view.setBackgroundResource(R.drawable.spear)
+            R.id.box_one_text -> view.setBackgroundResource(R.drawable.ic_looks_one_black)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_looks_two_black)
+            R.id.box_three_text -> view.setBackgroundResource(R.drawable.ic_looks_three_black)
+            R.id.box_four_text -> view.setBackgroundResource(R.drawable.ic_looks_four_black)
+            R.id.box_five_text -> view.setBackgroundResource(R.drawable.ic_looks_five_black)
 
             // Boxes using custom colors for background
             R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
